829d067db59e5482ee7dea0edb6539ea81edb520
linfeng <1029776125@qq.com>
2024-02-25 22:44:59 +0800

    init respority

---
+++ src/test/java/com/tw/gitpractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.gitpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
